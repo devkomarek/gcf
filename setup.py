4738f1615492f31910fbdd9ffd57d78d18774eaf
lukasz_szymszon <lukasz.szymszon@apilia.pl>
2019-12-16 14:27:56 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='google-cloud-functions',  
      packages = ['googlecloudfunctions'],
-     version='0.3.5',
+     version='0.3.6',
      author="Lukasz Szymszon",
      author_email="devkomarek@gmail.com",
      description="GCF utility package",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/devkomarek/google-cloud-functions",
-     download_url="https://github.com/devkomarek/google-cloud-functions/releases/tag/0.3.5",
+     download_url="https://github.com/devkomarek/google-cloud-functions/releases/tag/0.3.6",
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
